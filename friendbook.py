31291f32c8691864eff3c2c4842c4f64cef60879
n9br <andre.neugebauer@gmail.com>
2022-10-26 23:08:19 +0200

    added delete and update
    update throws problem with ucol Line 136. unresolved
    
    Traceback (most recent call last):
      File "c:\Users\an\Documents\GitHub\Projects\Friendbook\friendbook.py", line 220, in <module>
    ize_evaluate
        resolved_keys_as_propnames = cls._resolved_keys_as_propnames(
      File "C:\Users\an\AppData\Local\Programs\Python\Python310\lib\site-packages\sqlalchemy\orm\persistence.py", line 2057, in _resolved_keys_as_propnames
        raise sa_exc.InvalidRequestError(
    sqlalchemy.exc.InvalidRequestError: Invalid expression type: 'ucol'

--- friendbook.py
+++ friendbook.py
@@ -1,8 +1,15 @@
 from decimal import Decimal
-from turtle import window_height
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+import os
+from tkinter import commondialog
+from rich import print
+from rich.console import Console
+from rich.table import Table
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, update
 from sqlalchemy.orm import declarative_base, sessionmaker
+from turtle import window_height
 
+# rich console
+console = Console()
 
 # # # ###########################################
 # # Do not touch!
@@ -67,14 +74,39 @@ def database_add_friend(friend: Friend):
     Database command to add a new friend.
     """
     session.add(friend)
+    # ORM - Object Relational Mapper 
     session.commit()
+
+def database_delete_friend(id: str):
+    """
+    DB delete record
+    """
+    delFriend = session.get(Friend, id)
+    session.delete(delFriend)
+    session.commit()
+    # session.query.filter(User.id == 123).delete()
+
 
 def database_get_all_friends():
     """
     Database command to get all friends.
     """
+    #os.
     all_friends = session.query(Friend).all()
-    print(all_friends)
+    
+    table = Table(show_header=True, header_style="bold magenta")
+    table.add_column("id", style='dim')
+    table.add_column("FirstName")
+    table.add_column("LastName")
+    table.add_column("Phone")
+    table.add_column("Nick")
+    table.add_column("Address")
+
+    for friend in all_friends:
+    #    print(f"{friend.first_name} {friend.last_name}")
+        table.add_row(str(friend.id),friend.first_name,friend.last_name,friend.phone_nr,friend.nick_name,friend.address)
+    console.print(table)
+
 
 def add_friends():
     """
@@ -97,9 +129,70 @@ def add_friends():
     # new_friend = Friend(first_name = "Robert", last_name = "Kozljanic", phone_nr = "0157 24 88 357", nick_name = "Roberto", 
     #    address = "MUC", birthday = "30.3.1970", email = "roberto@gmail.com")
 
-    database_add_friend(new_friend)
-    
+    # !? Validierung ?!
 
+    database_add_friend(new_friend)
+
+def database_update_friend(id: int, ucol: str, ustr: str):
+    """
+    DB update record
+    """
+    session.execute(
+        update(Friend)
+        .where(Friend.id == id)
+        .values(ucol=ustr)
+    )
+    session.commit()
+
+def update_friends():
+    """
+    Freunde aktualisieren
+    """
+
+    updateId = input("Which ID do you want to update? :")
+    print("""
+        Which Column do you want to update ? Spell correctly.
+        first_name
+        last_name
+        phone_nr
+        nick_name
+        address
+        birthday
+        email
+    """
+    )
+    updateCol = input()
+    updateStr = input("Enter new value: ")
+
+    database_update_friend(updateId,updateCol,updateStr)
+
+
+def delete_friends():
+    """
+    select and delete friend
+    """
+       
+    delId = input("Which id do you want to delete? :")
+
+    database_delete_friend(str(delId))
+
+    
+def getUserInput():
+    print('''
+    What do you want to do ?
+
+    (a)dd user
+    (l)ist users
+    (d)elete user
+    (u)pdate user
+    (e)xit    
+    
+    ''')
+
+    userInput = input()
+    userInput = userInput.lower()
+
+    return(userInput)
 
 # # # ###########################################
 # # # Main
@@ -107,8 +200,24 @@ def add_friends():
 if __name__ == "__main__":
     initialize_database()
 
-    # add friends
-    add_friends() 
+    while True:
 
-    # Example to list all friends
-    database_get_all_friends()
+        userInput = getUserInput()
+
+        match userInput:
+            case 'e':
+                exit(1)
+
+            case 'a':
+                add_friends() 
+
+            case 'l':
+                # Example to list all friends
+                database_get_all_friends()
+            
+            case 'u':
+                database_get_all_friends()
+                update_friends()
+
+            case 'd':
+                delete_friends()
